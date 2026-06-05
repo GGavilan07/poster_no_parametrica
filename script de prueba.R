af9b1c6e2185b0c5b0525d3517eee3ce0c6a5e44
GGavilan07 <gavilangabriel6@gmail.com>
2026-06-04 22:37:30 -0400

    script actualizado 22:38 04-04-2026

--- script de prueba.R
+++ script de prueba.R
@@ -1,13 +1,15 @@
 library(tidyverse) #necesario para trabajar con los datos
 library(haven) #librería que permite leer datos provinientes de STATA  (.dta)
+library(Hmisc) #librería que permite calcular estadísticos con ponderaciones
+library(survey) #librería que permite trabajar con los diseños de las encuestas
 options(scipen = 999) #opción que muestra la extensión de los decimales
 file.choose() #escogemos el archivo para saber la ruta
 df <- read_dta("C:\\Users\\gabo\\Documents\\poster_no_parametrica\\casen_2024.dta") #leemos la base de datos
-media_segun_sexo <- df %>% #tomamos la base original
+datos <- df %>% #tomamos la base original
   filter(region == 7, y1 > 0) %>% #filtramos por la región del maule e y1 > 0
   mutate(sexo = case_when(sexo == 1 ~ "Hombre", #cambiamos la variable sexo por mera comodidad
-                   sexo == 2 ~ "Mujer"
-         )) %>%
+                          sexo == 2 ~ "Mujer"))
+media_segun_sexo <- datos %>% 
   group_by(sexo) %>% #agrupamos por sexo
   summarise(media_ingreso=mean(y1), n =n()) #tabla resumen de la media segun el sexo del salario liquido en su trabajo principal
 
@@ -15,11 +17,7 @@ media_segun_sexo <- df %>% #tomamos la base original
 
 #ahora calculamos la media que según la misma CASEN afirma que es correcta para realizar pruebas estadísticas significativas
 #en otras palabras, calcularemos la media con su respectivo factor de expansión regional
-media_ponderada <- df %>%
-  filter(region == 7, y1 > 0) %>% #filtramos por región y por las personas cuyos salarios del trabajo principal son mayores que 0
-  mutate(sexo = case_when( #modificamos la variable sexo para que nuestra tabla resumen sea clara.
-      sexo == 1 ~ "Hombre",
-      sexo == 2 ~ "Mujer")) %>%
+media_ponderada <- datos %>% 
   group_by(sexo) %>% #agrupamos por sexo
   summarise(media_ponderada = weighted.mean( #utilizamos weighted.mean() del paquete stats
       y1, #la variable a la cual queremos calcularle la media ponderada
@@ -30,46 +28,74 @@ media_ponderada
 
 # para preguntar el miércoles ------------------------------------------------------------------
 
-library(Hmisc)
+mediana_ponderada <- datos %>% 
+  group_by(sexo) %>% 
+  summarise(mediana= wtd.quantile(y1, w = expr, probs = 0.50))
+mediana_ponderada
 
-datos <- df %>%
-  filter(region == 7,
-         y1 > 0) %>%   mutate(sexo = case_when( #modificamos la variable sexo para que nuestra tabla resumen sea clara.
-           sexo == 1 ~ "Hombre",
-           sexo == 2 ~ "Mujer"))
 
-percentiles <- datos %>%
-  group_by(sexo) %>%
-  summarise(
-    p05 = wtd.quantile(y1, weights = expr, probs = 0.05),
-    p15 = wtd.quantile(y1, weights = expr, probs = 0.15),
-    P25 = wtd.quantile(y1, weights = expr, probs = 0.25),
-    P50 = wtd.quantile(y1, weights = expr, probs = 0.50),
-    P75 = wtd.quantile(y1, weights = expr, probs = 0.75),
-    P90 = wtd.quantile(y1, weights = expr, probs = 0.90),
-    p95 = wtd.quantile(y1, weights = expr, probs = 0.95),
-    p99 = wtd.quantile(y1, weights = expr, probs = 0.99)
-  )
-# Separar hombres y mujeres
-hombres <- percentiles %>% filter(sexo == "Hombre")
-mujeres <- percentiles %>% filter(sexo == "Mujer")
 
-# Tabla final para el póster
-tabla_brecha <- tibble(
-  Percentil = c("P05","P15","P25","P50","P75","P90","P95","P99"),
-  Hombre = as.numeric(hombres[1, 2:9]),
-  Mujer = as.numeric(mujeres[1, 2:9])
-) %>%
+# limpieza de los datos ---------------------------------------------------
+
+datos_a_limpiar <- datos %>% select(region, sexo, y1, expr, varunit, varstrat, id_vivienda, id_persona)
+conteo_na <- datos_a_limpiar %>% summarise(across(everything(), ~sum(is.na(.)))) #revisamos columna por columna todas las observaciones, buscando sumar todos los NA.
+conteo_na #0 Na
+# Buscamos duplicados usando los ids únicas de la CASEN
+duplicados_reales <- datos_a_limpiar %>%
+  group_by(id_vivienda, id_persona) %>% #Agrupamos por id único de vivivienda y persona
+  filter(n() > 1) #Filtramos por cada id_vivienda e id_persona que esté presente más de una vez
+  sum(duplicated(duplicados_reales[c("id_persona", "id_vivienda")])) #sumamos cada id_persona e id_vivienda que esté repetido
+df_sin_duplicados <- datos_a_limpiar %>% 
+  distinct(id_vivienda, id_persona, .keep_all = TRUE)
+#con distinct seleccionamos todos los id_vivienda e id_persona que no se repiten. Y .keep asegura que persistan los demás ids
+
+# boxplot para ver sesgo --------------------------------------------------
+library(ggplot2)
+ggplot(data = df_sin_duplicados, aes(x= sexo, y=y1, fill = sexo)) +
+  geom_boxplot(alpha=0.7)+
+  scale_fill_manual(values = c("Hombre" = "lightblue", "Mujer" = "lightpink"))+
+  labs(
+    title= "Boxplots de Salario por Ocupación Principal según Sexo",
+    x = "Sexo",
+    y= "Salario por Ocupación Principal",
+  ) +
+  theme_minimal() +
+  theme(legend.position = "right")
+
+# resumen descriptivo -----------------------------------------------------
+
+
+#Confirmamos sesgo a la derecha en ambos casos, procedemos a log-normalizar.
+# log-normal para atípicos ------------------------------------------------
+##Importante preguntarle al profe si es necesario realizar un test de log-normalidad a los datos
+##Log-normalizamos porque son datos sesgados a la derecha.
+##FUNDAMENTAR CON EVIDENCIA EMPÍRICA (buscar estudios que hayan hecho esto)
+
+##Atípicos
+datos_log <- df_sin_duplicados %>%
   mutate(
-    Brecha_Porcentual = round(100 * (Hombre - Mujer) / Mujer, 1)
+    log_y1 = log(y1),
+    media_log = mean(log_y1),
+    sd_log = sd(log_y1),
+    # Detectamos a más de 3 desviaciones estándar, pero en escala logarítmica
+    es_atipico_log = abs(log_y1 - media_log) > (3 * sd_log)
   )
 
-tabla_brecha
-tabla_poster <- tabla_brecha %>%
-  mutate(
-    Hombre = format(round(Hombre, 0), big.mark = ".", scientific = FALSE),
-    Mujer = format(round(Mujer, 0), big.mark = ".", scientific = FALSE),
-    Brecha_Porcentual = paste0(Brecha_Porcentual, "%")
-  )
+table(datos_log$es_atipico_log)
+atipicos <- datos_log %>% filter(es_atipico_log == TRUE)
+atipicos
+##Trabajaremos con los atípicos incluidos, entonces:
+datos_limpios <- df_sin_duplicados
+diseno_casen <- svydesign(
+  ids = ~varunit,      # Conglomerados de varianza
+  strata = ~varstrat,  # Estratos de varianza
+  weights = ~expr,     # Factor de expansión regional
+  data = datos_limpios,
+  nest = TRUE
+)
 
-tabla_poster
+# 4. Calcular la mediana poblacional exacta por sexo
+medianas_sexo <- datos_limpios %>% group_by(sexo) %>% 
+  summarise(wtd.quantile(y1, w=expr, probs = 0.50))
+medianas_sexo
+svyranktest(y1~sexo, diseno_casen, test = "wilcoxon")
